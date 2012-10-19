5aa7d301459685cad3d61b53ecd016973f574659
Guillaume Seguin <guillaume@segu.in>
2012-10-19 18:03:56 +0200

    Add labels support for plots

--- templates/script_plot.js
+++ templates/script_plot.js
@@ -1,7 +1,8 @@
 var r = Raphael("plotholder{{ itemid }}");
+var div = $("#plotholder{{ itemid }}");
 var chart = r.linechart(
     10, 10,
-    390, 180,
+    div.width() - 10, div.height() - 20,
     {{ item.xdata }},
     {{ item.ydata }},
     {
@@ -9,4 +10,18 @@ var chart = r.linechart(
        smooth: true,
        {% if item.options %}{{ item.options }}{% endif %}
      });
-
+{% if item.labels %}
+var labels = [{% for label in item.labels %}"{{ label }}", {% endfor %}];
+chart.labels = r.set();
+var x = 15; var h = 5;
+for( var i = 0; i < labels.length; ++i ) {
+  var clr = chart.lines[i].attr("stroke");
+  chart.labels.push(r.set());
+  chart.labels[i].push(r["circle"](x + 5, h, 5)
+                       .attr({fill: clr, stroke: "none"}));
+  chart.labels[i].push(txt = r.text(x + 15, h, labels[i])
+                       .attr(Raphael.g.txtattr)
+                       .attr({fill: "#000", "text-anchor": "start"}));
+  x += 15 + txt.getBBox().width + 10;
+};
+{% endif %}
