f5e06fb3a66dd2febf924357a74191f463eec573
Guillaume Seguin <guillaume@segu.in>
2013-09-20 18:11:06 +0200

    Only generate thumbnails if needed

--- webpagemaker/thumbnail.py
+++ webpagemaker/thumbnail.py
@@ -6,7 +6,8 @@ def make_thumbnail(item):
     new_name = os.path.join(dirname, "thumbnail_%d_%d_%s" % (item["width"], item["height"], basename))
     dirname, basename = os.path.split(item["rawpath"])
     new_path = os.path.join(dirname, "thumbnail_%d_%d_%s" % (item["width"], item["height"], basename))
-    image = Image.open(item["rawpath"])
-    image.thumbnail((item["width"], item["height"]))
-    image.save(new_path)
+    if not os.path.exists(new_path) or (os.path.getmtime(item["rawpath"]) > os.path.getmtime(new_path)):
+        image = Image.open(item["rawpath"])
+        image.thumbnail((item["width"], item["height"]))
+        image.save(new_path)
     return new_name
