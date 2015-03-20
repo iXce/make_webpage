0f4dbe138b806a493c3d5b0c86050f32660a318b
Guillaume Seguin <guillaume@segu.in>
2015-03-20 18:14:41 +0100

    Fix silly leftover

--- templates/script_table.js
+++ templates/script_table.js
@@ -4,9 +4,7 @@
                 {% include "script_type_switch.html" %}
             {% endfor %}
     {% endif %}
-    <tbody>
     {% for item0 in item.rows %}
-        <tr>
         {% if item0 is sequence and not item0 is string and not item0 is mapping %}
             {% for item in item0 %}
                 {% include "script_type_switch.html" %}
